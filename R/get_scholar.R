2367fa96c97b853e293729c99369d81d5accd0d5
dataning <bnnl@leeds.ac.uk>
2020-05-27 16:37:58 +0100

    1

--- R/get_scholar.R
+++ R/get_scholar.R
@@ -1,16 +1,24 @@
+#' Title
+#'
+#' @param name
+#'
+#' @return
+#' @export
+#'
+#' @examples
 get_scholar <- function(name){
-  
+
   pacman::p_load(httr, rvest, scholar)
-  
+
   name <- gsub(" ", "+", name, fixed = TRUE)
-  
-  text <- paste0("https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q=", name) %>% 
-    read_html() %>% 
+
+  text <- paste0("https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q=", name) %>%
+    read_html() %>%
     html_nodes(., ".gs_rt2")
-  
-  dcat <- strsplit(as.character(text), "(user=)|&")[[1]][2] %>% 
+
+  dcat <- strsplit(as.character(text), "(user=)|&")[[1]][2] %>%
     get_profile()
-  
+
   return(dcat)
-  
+
 }
